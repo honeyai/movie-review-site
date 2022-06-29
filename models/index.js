b16a80826b851ebeebe9ef1acfecf8b98c442ec5
Ben Grinthal <bgrinthal@gmail.com>
2022-06-29 10:32:58 -0400

    added .gitignore file

--- models/index.js
+++ models/index.js
@@ -3,6 +3,33 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 //relationships (hasMany, belongsTo, etc...)
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+})
 
 module.exports = { User, Post, Comment };
