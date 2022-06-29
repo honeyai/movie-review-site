39f87bed5148e136693054e1c283113468a13427
Ben Grinthal <bgrinthal@gmail.com>
2022-06-29 10:10:24 -0400

    initial push

---
+++ models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+//relationships (hasMany, belongsTo, etc...)
+
+
+module.exports = { User, Post, Comment };
