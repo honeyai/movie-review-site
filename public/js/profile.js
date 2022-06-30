01dc174a02be5f46a3496b50fb06d96cef0bb85c
Ben Grinthal <bgrinthal@gmail.com>
2022-06-30 18:25:38 -0400

    fixed error in seed file

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,14 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const username = document.querySelector('#project-name').value.trim();
+  // const needed_funding = document.querySelector('#project-funding').value.trim();
+  const review = document.querySelector('#project-desc').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+  if (username && review) {
+    const response = await fetch(`/api/posts`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ username, review }),
       headers: {
         'Content-Type': 'application/json',
       },
