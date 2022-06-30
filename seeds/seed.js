3b930e8a5b7cca7ead48be71f015dfecee6c1764
Ben Grinthal <bgrinthal@gmail.com>
2022-06-30 16:52:59 -0400

    used axios to fetch api

--- seeds/seed.js
+++ seeds/seed.js
@@ -20,39 +20,4 @@ const seedDatabase = async () => {
     process.exit(0);
   };
 
-
-// const seedDatabase = async () => {
-//   await sequelize.sync({ force: true });
-
-//   const users = await User.bulkCreate(userData, {
-//     individualHooks: true,
-//     returning: true,
-//   });
-
-//   // const posts = await Post.bulkCreate(postData, {
-//   //   individualHooks: true,
-//   //   returning: true,
-//   // });
-
-//   for (const post of postData) {
-//     await Post.create({
-//       ...post,
-//       //user_id & post_id must be linked somehow (i think)
-//       user_id: users[Math.floor(Math.random() * users.length)].id,
-//     });
-//   }
-
-
-//   // for (const comment of commentData) {
-//   //   await Comment.create({
-//   //     ...comment,
-//   //     //user_id & post_id must be linked somehow (i think)
-//   //     user_id: users[Math.floor(Math.random() * users.length)].id,
-//   //     post_id: posts[Math.floor(Math.random() * posts.length)].id
-//   //   });
-//   // }
-
-//   process.exit(0);
-// };
-
 seedDatabase();
