5655d1b1c3003e320f5d9945eddea3eea6984834
honeyai <contact.jessicasanchez@gmail.com>
2022-07-01 10:35:48 -0400

    api passport routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,60 +1,37 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const passport = require("../../config/passport");
 
-// /api/user route
-router.post('/', async (req, res) => {
+router.post('/login', passport.authenticate("local"), async (req, res) => {
     try {
-        const userData = await User.create(req.body);
-        console.log(userData);
-
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-
-            res.status(200).json(userData);
-        });
+        res
+            .status(200)
+            .json({ user: req.user, message: "You are now logged in!" });
     } catch (err) {
-        res.status(400).json(err);
+        console.log(err);
+        res.status(401).json(err);
     }
 });
 
-router.post('/login', async (req, res) => {
+router.post('./signup', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { email: req.body.email } });
-
-        if (!userData) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password, please try again' });
-            return;
-        }
-
-        const validPassword = await userData.checkPassword(req.body.password);
-
-        if (!validPassword) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password, please try again' });
-            return;
-        }
-
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-
-            res.json({ user: userData, message: 'You are now logged in!' });
+        const dbUserData = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
         });
-
     } catch (err) {
-        res.status(400).json(err);
+        console.log(err.errors[0]);
+        res.status(500).json({
+            message: err.errors[0]["message"]
+        });
     }
-});
+})
 
 router.post('/logout', (req, res) => {
-    if (req.session.logged_in) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
+    if(req.user) {
+        req.logout();
+        res.redirect("/");
     } else {
         res.status(404).end();
     }
