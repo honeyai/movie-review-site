e5b3e780676866b5709328536909ad2783af62d7
Ben Grinthal <bgrinthal@gmail.com>
2022-07-03 18:58:26 -0400

    udpated handlebars and comment section

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,35 +1,34 @@
 const newCommentHandler = async (event) => {
-    event.preventDefault();
-  
-    const commentText = document.querySelector('#comment-text').value.trim();
-    // const needed_funding = document.querySelector('#project-funding').value.trim();
-    // const review = document.querySelector('#project-desc').value.trim();
-  
-    if (commentText) {
-      const response = await fetch(`/api/comments`, {
-        method: 'POST',
-        body: JSON.stringify({ commentText }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/posts');
-      } else {
-        alert('Failed to create comment');
-      }
+  event.preventDefault();
+  const comment_text = document.querySelector('#comment-text').value.trim();
+  // const needed_funding = document.querySelector('#project-funding').value.trim();
+  // const review = document.querySelector('#project-desc').value.trim();
+
+  if (comment_text) {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({ comment_text }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/post');
+    } else {
+      alert('Failed to create comment');
     }
-  };
-  
+  }
+};
+
 //   const delButtonHandler = async (event) => {
 //     if (event.target.hasAttribute('data-id')) {
 //       const id = event.target.getAttribute('data-id');
-  
+
 //       const response = await fetch(`/api/posts/${id}`, {
 //         method: 'DELETE',
 //       });
-  
+
 //       if (response.ok) {
 //         document.location.replace('/profile');
 //       } else {
@@ -37,12 +36,11 @@ const newCommentHandler = async (event) => {
 //       }
 //     }
 //   };
-  
-  document
-    .querySelector('.new-comment-form')
-    .addEventListener('submit', newCommentHandler);
-  
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newCommentHandler);
+
 //   document
 //     .querySelector('.comment-list')
 //     .addEventListener('click', delButtonHandler);
-  
