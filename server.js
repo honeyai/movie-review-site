d45a1f0b7e9f9dc920a91abd010394ffad88c85c
Ben Grinthal <bgrinthal@gmail.com>
2022-07-05 15:31:26 -0400

    comment out session

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const sess = {
   cookie: {},
   resave: false,
   saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+  // store: new SequelizeStore({
+  //   db: sequelize
+  // })
 };
 
 app.use(session(sess));
